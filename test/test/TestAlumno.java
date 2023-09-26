ee32a66abd20c67f626e629d5b3f8054165a3588
Juan-Pablo-Caldas <jcaldas728@alumno.unlam.edu.ar>
2023-09-26 17:15:12 -0300

    agregue_un_test_mas_en_alumnos

--- test/test/TestAlumno.java
+++ test/test/TestAlumno.java
@@ -1,6 +1,5 @@
 package test;
 
-
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -15,36 +14,53 @@ import clases.Turnos;
 public class TestAlumno {
 
 	@Test
-    public void queSePuedaAsignarMateriaAprobada() {
-        // PREPARACION
-        String nombreAlum = "Victoria", apellidoAlum = "Junco", nombreMat = "PB2";
-        Integer dniAlum = 111, idMat = 5;
-        Boolean resultadoDeLaEjecucion;
+	public void queSePuedaAsignarMateriaAprobada() {
+		// PREPARACION
+		String nombreAlum = "Victoria", apellidoAlum = "Junco", nombreMat = "PB2";
+		Integer dniAlum = 111, idMat = 5;
+		Boolean resultadoDeLaEjecucion;
 
-        // EJECUCION
-        Alumno alumno = new Alumno(nombreAlum, apellidoAlum, dniAlum);
-        Materia materia = new Materia(nombreMat, idMat);
-        resultadoDeLaEjecucion = alumno.asignarMateriasAprobadas(materia);
+		// EJECUCION
+		Alumno alumno = new Alumno(nombreAlum, apellidoAlum, dniAlum);
+		Materia materia = new Materia(nombreMat, idMat);
+		resultadoDeLaEjecucion = alumno.asignarMateriasAprobadas(materia);
 
-        // VALIDACION
-        assertTrue(resultadoDeLaEjecucion);
-    }
-@Test
-    public void queSePuedaAsignarComision() {
-        // PREPARACION
-        String nombreAlum = "Victoria", apellidoAlum = "Junco", nombreMat = "PB2";
-        Integer dniAlum = 111, idMat = 5;
-        Boolean resultadoDeLaEjecucion;
+		// VALIDACION
+		assertTrue(resultadoDeLaEjecucion);
+	}
 
-        // EJECUCION
-        Alumno alumno = new Alumno(nombreAlum, apellidoAlum, dniAlum);
-        Materia materia = new Materia(nombreMat, idMat);
-        CicloLectivo ciclo = new CicloLectivo(45, 2023, 4, 20, 12, 20);
-        Comision comision = new Comision(15, materia, ciclo, Turnos.MAÑANA, Dia.JUEVES);
-        resultadoDeLaEjecucion = alumno.asignarComision(comision);
+	@Test
+	public void queSePuedaAsignarComision() {
+		// PREPARACION
+		String nombreAlum = "Victoria", apellidoAlum = "Junco", nombreMat = "PB2";
+		Integer dniAlum = 111, idMat = 5;
+		Boolean resultadoDeLaEjecucion;
 
-        // VALIDACION
-        assertTrue(resultadoDeLaEjecucion);
-    }
+		// EJECUCION
+		Alumno alumno = new Alumno(nombreAlum, apellidoAlum, dniAlum);
+		Materia materia = new Materia(nombreMat, idMat);
+		CicloLectivo ciclo = new CicloLectivo(45, 2023, 4, 20, 12, 20);
+		Comision comision = new Comision(15, materia, ciclo, Turnos.MAÑANA, Dia.JUEVES);
+		resultadoDeLaEjecucion = alumno.asignarComision(comision);
+
+		// VALIDACION
+		assertTrue(resultadoDeLaEjecucion);
+	}
+
+	@Test
+	public void queSePuedaAsignarMateriaFaltanteParaCursar() {
+		// PREPARACION
+		String nombreAlum = "Victoria", apellidoAlum = "Junco", nombreMat = "PB2";
+		Integer dniAlum = 111, idMat = 5;
+		Boolean resultadoDeLaEjecucion;
+
+		// EJECUCION
+		Alumno alumno = new Alumno(nombreAlum, apellidoAlum, dniAlum);
+		Materia materia = new Materia(nombreMat, idMat);
+		resultadoDeLaEjecucion = alumno.asignarMateriaFaltanteParaCursar(materia);
+
+		// VALIDACION
+		assertTrue(resultadoDeLaEjecucion);
+	}
 
 }
