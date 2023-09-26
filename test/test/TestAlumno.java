5a4178ff383173cdbb4f6fdb66eaa5f859306e81
vic-jun <vjunco304@alumno.unlam.edu.ar>
2023-09-26 17:13:26 -0300

    agregue_dos_test

--- test/test/TestAlumno.java
+++ test/test/TestAlumno.java
@@ -14,6 +14,37 @@ import clases.Turnos;
 
 public class TestAlumno {
 
-	
+	@Test
+    public void queSePuedaAsignarMateriaAprobada() {
+        // PREPARACION
+        String nombreAlum = "Victoria", apellidoAlum = "Junco", nombreMat = "PB2";
+        Integer dniAlum = 111, idMat = 5;
+        Boolean resultadoDeLaEjecucion;
+
+        // EJECUCION
+        Alumno alumno = new Alumno(nombreAlum, apellidoAlum, dniAlum);
+        Materia materia = new Materia(nombreMat, idMat);
+        resultadoDeLaEjecucion = alumno.asignarMateriasAprobadas(materia);
+
+        // VALIDACION
+        assertTrue(resultadoDeLaEjecucion);
+    }
+@Test
+    public void queSePuedaAsignarComision() {
+        // PREPARACION
+        String nombreAlum = "Victoria", apellidoAlum = "Junco", nombreMat = "PB2";
+        Integer dniAlum = 111, idMat = 5;
+        Boolean resultadoDeLaEjecucion;
+
+        // EJECUCION
+        Alumno alumno = new Alumno(nombreAlum, apellidoAlum, dniAlum);
+        Materia materia = new Materia(nombreMat, idMat);
+        CicloLectivo ciclo = new CicloLectivo(45, 2023, 4, 20, 12, 20);
+        Comision comision = new Comision(15, materia, ciclo, Turnos.MAÑANA, Dia.JUEVES);
+        resultadoDeLaEjecucion = alumno.asignarComision(comision);
+
+        // VALIDACION
+        assertTrue(resultadoDeLaEjecucion);
+    }
 
 }
