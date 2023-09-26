11bf340392088e88851cba56cf2f018cb79a15aa
Juan-Pablo-Caldas <jcaldas728@alumno.unlam.edu.ar>
2023-09-26 17:38:21 -0300

    agregue3testEnUniversidad

--- test/test/TestUniversidad.java
+++ test/test/TestUniversidad.java
@@ -19,7 +19,48 @@ import clases.Turnos;
 import clases.Universidad;
 
 public class TestUniversidad {
+	@Test
+	public void queSePuedaIngresarUnaUniversidad() {
+		// PREPARACION
+		String nombre = "UNLAM";
+		// EJECUCION
+		Universidad unlam = new Universidad(nombre);
+		// VALIDACION
+		assertNotNull(unlam);
 
-	
+	}
+
+	@Test
+	public void queSePuedaIngresarAlumno() {
+		// PREPARACION
+		String nombre = "UNLAM", nombreAlumno = "Juan", apellidoAlumno = "Caldas";
+		Integer dniAlumno = 222;
+		// EJECUCION
+		Universidad unlam = new Universidad(nombre);
+		Alumno alumno = new Alumno(nombreAlumno, apellidoAlumno, dniAlumno);
+		Boolean ingresoElAlumno;
+
+		ingresoElAlumno = unlam.ingresarAlumno(alumno);
+		// VALIDACION
+		assertTrue(ingresoElAlumno);
+	}
+
+	@Test
+	public void queNoSePuedaIngresarAlumnosConMismosDNI() {
+		// PREPARACION
+		String nombre = "UNLAM", nombreAlumno = "Juan", apellidoAlumno = "Caldas";
+		Integer dniAlumno = 222, numeroEsperado = 1;
+		// EJECICION
+		Universidad unlam = new Universidad(nombre);
+		Alumno alumno = new Alumno(nombreAlumno, apellidoAlumno, dniAlumno);
+		Alumno alumno2 = new Alumno(nombreAlumno, apellidoAlumno, dniAlumno);
+		Boolean ingresoElAlumno;
+
+		ingresoElAlumno = unlam.ingresarAlumno(alumno);
+		ingresoElAlumno = unlam.ingresarAlumno(alumno2);
+		// VALIDACION
+		assertFalse(ingresoElAlumno);
+		assertEquals(numeroEsperado, unlam.getCantidadDeAlumnos());
+	}
 
 }
