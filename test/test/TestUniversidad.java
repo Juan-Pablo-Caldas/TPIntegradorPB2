31042f7e0af300be37717bb2c7f5af1f792b68fb
Juan-Pablo-Caldas <jcaldas728@alumno.unlam.edu.ar>
2023-09-26 17:40:41 -0300

    agregue3testMas

--- test/test/TestUniversidad.java
+++ test/test/TestUniversidad.java
@@ -62,62 +62,141 @@ public class TestUniversidad {
 		assertFalse(ingresoElAlumno);
 		assertEquals(numeroEsperado, unlam.getCantidadDeAlumnos());
 	}
-	
+
 	@Test
-    public void queSePuedaIngresarProfe() {
-        // PREPARACION
-        String nombre = "UNLAM", nombreProfe = "Diego", apellidoProfe = "Diaz";
-        Integer legajoProfe = 444;
-        // EJECUCION
-        Universidad unlam = new Universidad(nombre);
-        Profe profesor = new Profe(nombreProfe, apellidoProfe, legajoProfe);
-        Boolean ingresoElProfe;
+	public void queSePuedaIngresarProfe() {
+		// PREPARACION
+		String nombre = "UNLAM", nombreProfe = "Diego", apellidoProfe = "Diaz";
+		Integer legajoProfe = 444;
+		// EJECUCION
+		Universidad unlam = new Universidad(nombre);
+		Profe profesor = new Profe(nombreProfe, apellidoProfe, legajoProfe);
+		Boolean ingresoElProfe;
 
-        ingresoElProfe = unlam.ingresarProfesor(profesor);
-        // VALIDACION
-        assertTrue(ingresoElProfe);
+		ingresoElProfe = unlam.ingresarProfesor(profesor);
+		// VALIDACION
+		assertTrue(ingresoElProfe);
 
-    }
-@Test
-    public void queNoSePuedaIngresarProfesoresConMismoDNI() {
-        // PREPARACION
+	}
 
-        String nombre = "UNLAM", nombreProfe = "Diego", apellidoProfe = "Diaz";
-        Integer dni = 444, numeroEsperado = 1;
+	@Test
+	public void queNoSePuedaIngresarProfesoresConMismoDNI() {
+		// PREPARACION
 
-        // EJECUCION
+		String nombre = "UNLAM", nombreProfe = "Diego", apellidoProfe = "Diaz";
+		Integer dni = 444, numeroEsperado = 1;
 
-        Universidad unlam = new Universidad(nombre);
-        Profe profesor = new Profe(nombreProfe, apellidoProfe, dni);
-        Profe profesor2 = new Profe(nombreProfe, apellidoProfe, dni);
-        Boolean ingresoElProfe;
+		// EJECUCION
 
-        unlam.ingresarProfesor(profesor);
-        ingresoElProfe = unlam.ingresarProfesor(profesor2);
+		Universidad unlam = new Universidad(nombre);
+		Profe profesor = new Profe(nombreProfe, apellidoProfe, dni);
+		Profe profesor2 = new Profe(nombreProfe, apellidoProfe, dni);
+		Boolean ingresoElProfe;
 
-        // VALIDACION
+		unlam.ingresarProfesor(profesor);
+		ingresoElProfe = unlam.ingresarProfesor(profesor2);
 
-        assertFalse(ingresoElProfe);
-        assertEquals(numeroEsperado, unlam.getCantidadDeProfesores());
-    }
-@Test
-    public void queSePuedaIngresarMateria() {
-        // PREPARACION
+		// VALIDACION
 
-        String nombre = "UNLAM", nombreMateria = "PB2";
-        Integer codigoMateria = 88;
+		assertFalse(ingresoElProfe);
+		assertEquals(numeroEsperado, unlam.getCantidadDeProfesores());
+	}
 
-        // EJECUCION
+	@Test
+	public void queSePuedaIngresarMateria() {
+		// PREPARACION
 
-        Universidad unlam = new Universidad(nombre);
-        Materia materia = new Materia(nombreMateria, codigoMateria);
-        Boolean ingresoLaMateria;
+		String nombre = "UNLAM", nombreMateria = "PB2";
+		Integer codigoMateria = 88;
 
-        ingresoLaMateria = unlam.ingresarMateria(materia);
+		// EJECUCION
 
-        // VALIDACION
+		Universidad unlam = new Universidad(nombre);
+		Materia materia = new Materia(nombreMateria, codigoMateria);
+		Boolean ingresoLaMateria;
 
-        assertTrue(ingresoLaMateria);
-    }
+		ingresoLaMateria = unlam.ingresarMateria(materia);
+
+		// VALIDACION
+
+		assertTrue(ingresoLaMateria);
+	}
+
+	@Test
+	public void queNoSePuedaIngresarMateriasConMismoId() {
+		// PREPARACION
+
+		String nombre = "UNLAM", nombreMateria = "PB2";
+		Integer codigoMateria = 88, numeroEsperado = 1;
+
+		// EJECUCION
+
+		Universidad unlam = new Universidad(nombre);
+		Materia materia = new Materia(nombreMateria, codigoMateria);
+		Materia materia2 = new Materia(nombreMateria, codigoMateria);
+		Boolean ingresoLaMateria;
+
+		ingresoLaMateria = unlam.ingresarMateria(materia);
+		ingresoLaMateria = unlam.ingresarMateria(materia2);
+
+		// VALIDACION
+
+		assertFalse(ingresoLaMateria);
+		assertEquals(numeroEsperado, unlam.getCantidadDeMaterias());
+	}
+
+	@Test
+	public void queSePuedaIngresarComision() {
+		// PREPARACION
+
+		String nombre = "UNLAM", nombreMateria = "PB2", diaCurso = "Miercoles";
+		Integer codigoComision = 88, codigoMateria = 444, id = 20, anio = 2023, mesInicio = 4, diaInicio = 12,
+				mesFinalizacion = 8, diaFinalizacion = 27;
+		Turnos turno = Turnos.MAÑANA;
+		Dia dia = Dia.SABADO;
+		Boolean ingresoLaComision;
+
+		// EJECUCION
+
+		Universidad unlam = new Universidad(nombre);
+		Materia materia = new Materia(nombreMateria, codigoMateria);
+		CicloLectivo ciclo = new CicloLectivo(id, anio, mesInicio, diaInicio, mesFinalizacion, diaFinalizacion);
+		Comision comision = new Comision(codigoComision, materia, ciclo, turno, dia);
+
+		ingresoLaComision = unlam.ingresarComision(comision);
+
+		// VALIDACION
+
+		assertTrue(ingresoLaComision);
+	}
+
+	@Test
+	public void queNoSePuedaIngresarComisionesConLaMismaMateriaMismoCicloYMismoTurno() {
+		// PREPARACION
+
+		String nombre = "UNLAM", nombreMateria = "PB2";
+		Integer codigoComision = 88, codigoMateria = 444, esperado = 1, id = 20, anio = 2023, mesInicio = 4,
+				diaInicio = 12, mesFinalizacion = 8, diaFinalizacion = 27;
+
+		Turnos turno = Turnos.MAÑANA;
+		Dia dia = Dia.SABADO;
+		Boolean ingresoLaComision;
+
+		// EJECUCION
+
+		Universidad unlam = new Universidad(nombre);
+		Materia materia = new Materia(nombreMateria, codigoMateria);
+		CicloLectivo ciclo = new CicloLectivo(id, anio, mesInicio, diaInicio, mesFinalizacion, diaFinalizacion);
+		Comision comision = new Comision(codigoComision, materia, ciclo, turno, dia);
+		Comision comision2 = new Comision(codigoComision, materia, ciclo, turno, dia);
+
+		ingresoLaComision = unlam.ingresarComision(comision);
+		ingresoLaComision = unlam.ingresarComision(comision2);
+
+		// VALIDACION
+
+		assertFalse(ingresoLaComision);
+		assertEquals(esperado, unlam.getCantidadDeComisiones());
+	}
 
 }
