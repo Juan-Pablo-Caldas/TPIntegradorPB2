656a7e09738cbdd907b297b759e8b7e866946113
vic-jun <vjunco304@alumno.unlam.edu.ar>
2023-09-26 17:35:34 -0300

    agregue_dos_test_en_materia

--- test/test/TestMateria.java
+++ test/test/TestMateria.java
@@ -8,6 +8,37 @@ import clases.Materia;
 
 public class TestMateria {
 
-	
+	@Test
+    public void queSePuedaAgregarUnaMateriaCorrelativa() {
+        // PREPARACION
+        String nombreMat = "PB2", nombreCorrelativa = "PB1";
+        Integer idMateria = 1231, idCorrelativa = 4564;
+        Boolean resultadoDeLaEjecucion;
+
+        // EJECUCION
+        Materia materia = new Materia(nombreMat, idMateria);
+        Materia correlativa = new Materia(nombreCorrelativa, idCorrelativa);
+        resultadoDeLaEjecucion = materia.agregarMateriasCorrelativas(correlativa);
+
+        // VALIDACION
+        assertTrue(resultadoDeLaEjecucion);
+    }
+
+    @Test
+    public void queNoSePuedaAsignarCorrelativa() {
+        // PREPARACION
+        Integer id = 20, id2 = 30;
+        String nombre = "PB2";
+        Boolean resultado;
+
+        // EJECUCION
+        Materia materia = new Materia(nombre, id);
+
+        resultado = materia.asignarCorrelativa(id, id2);
+
+        // VALIDACION
+        assertFalse(resultado);
+    }
+
 
 }
