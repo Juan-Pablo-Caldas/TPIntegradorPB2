c37c81e4b25aecda0c5bea676a0265533445bc98
vic-jun <vjunco304@alumno.unlam.edu.ar>
2023-09-26 17:32:12 -0300

    agregue_dos_test_en_aula

--- test/test/TestAula.java
+++ test/test/TestAula.java
@@ -8,6 +8,33 @@ import clases.Aula;
 
 public class TestAula {
 
-	
+	@Test
+    public void queSePuedaAsignarCapacidadEntre40y100AUnAula() {
+        // PREPARACION
+        Integer numAula = 1, capacidadMaximaAula = 100, capacidadActual = 50, capacidadEsperada;
+
+        // EJECUCION
+        Aula aula = new Aula(capacidadMaximaAula, numAula);
+        aula.asignarCapacidad(capacidadActual);
+        capacidadEsperada = aula.getCapacidad();
+
+        // VALIDACION
+        assertEquals(capacidadActual, capacidadEsperada);
+    }
+
+    @Test
+    public void queNoSePuedaAsignarCapacidadMenorDe40yMayorDe100AUnAula() {
+        // PREPARACION
+        Integer numAula = 1, capacidadMaximaAula = 100, capacidadActual = 300, capacidadEsperada;
+
+        // EJECUCION
+        Aula aula = new Aula(capacidadMaximaAula, numAula);
+        aula.asignarCapacidad(capacidadActual);
+        capacidadEsperada = aula.getCapacidad();
+
+        // VALIDACION
+        assertNotEquals(capacidadActual, capacidadEsperada);
+    }
+
 
 }
