8c3e4bf7eba661a727a28a126455842ebd45e852
Juan-Pablo-Caldas <jcaldas728@alumno.unlam.edu.ar>
2023-09-26 17:35:55 -0300

    agregueLosTestDeNota

--- test/test/TestNota.java
+++ test/test/TestNota.java
@@ -9,6 +9,34 @@ import clases.TipoNota;
 
 public class TestNota {
 
-	
+	@Test
+	public void queSePuedaAsignarValorDeNota() {
+		// PREPARACION
+		Double valor = 7.0;
+		TipoNota tipo = TipoNota.PRIMERPARCIAL;
+		Boolean resultadoDeLaEjecucion;
+
+		// EJECUCION
+		Nota nota = new Nota(tipo);
+		resultadoDeLaEjecucion = nota.asignarValorDeNota(valor);
+
+		// VALIDACION
+		assertTrue(resultadoDeLaEjecucion);
+	}
+
+	@Test
+	public void queNoSePuedaAsignarValorDeNotaYaQueNoEstaEnParametros() {
+		// PREPARACION
+		Double valor = 11.0;
+		TipoNota tipo = TipoNota.PRIMERPARCIAL;
+		Boolean resultadoDeLaEjecucion;
+
+		// EJECUCION
+		Nota nota = new Nota(tipo);
+		resultadoDeLaEjecucion = nota.asignarValorDeNota(valor);
+
+		// VALIDACION
+		assertFalse(resultadoDeLaEjecucion);
+	}
 
 }
