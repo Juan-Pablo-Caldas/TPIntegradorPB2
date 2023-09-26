794ec7508894ef8b696b3bc367a436189bd4de36
vic-jun <vjunco304@alumno.unlam.edu.ar>
2023-09-26 17:37:40 -0300

    agregue_dos_test_en_profe

--- test/test/TestProfe.java
+++ test/test/TestProfe.java
@@ -14,6 +14,26 @@ import clases.Turnos;
 
 public class TestProfe {
 
-	
+	@Test
+    public void queSePuedaIngresarUnaComision() {
+        // PREPARACION
+        String nombreMat = "PB2", nombreProf = "Andres", apellidoProf = "Borgeat";
+        Integer codigoMat = 1231, numAula = 1, capacidadMaximaAula = 100, legajoProf = 456, idCiclo = 45,
+                anioCiclo = 2023, mesInicioCiclo = 3, diaInicioCiclo = 12, mesFinalCiclo = 7, diaFinalCiclo = 15;
+        Boolean resultadoDeLaEjecucion;
+
+        // EJECUCION
+        Materia materia = new Materia(nombreMat, codigoMat);
+        Aula aula = new Aula(numAula, capacidadMaximaAula);
+        aula.asignarCapacidad(50);
+        CicloLectivo ciclo = new CicloLectivo(idCiclo, anioCiclo, mesInicioCiclo, diaInicioCiclo, mesFinalCiclo,
+                diaFinalCiclo);
+        Comision comision = new Comision(222, materia, ciclo, Turnos.MAÑANA, Dia.JUEVES);
+        Profe profesor = new Profe(nombreProf, apellidoProf, legajoProf);
+        resultadoDeLaEjecucion = profesor.agregarComisionesAlProfesor(comision);
+
+        // VALIDACION
+        assertTrue(resultadoDeLaEjecucion);
+    }
 
 }
